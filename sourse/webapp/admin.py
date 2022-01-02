8408f24ed619da31394cd0ee2d7642955588be00
NiiazovMaksat <niyazov37@gmail.com>
2022-01-03 00:02:59 +0600

    добавлена миграция и стили Бутстрап для всех страниц

--- sourse/webapp/admin.py
+++ sourse/webapp/admin.py
@@ -8,7 +8,7 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'task', 'status', 'updated_at']
     list_filter = ['status']
     search_fields = ['task', 'updated_at']
-    fields = ['task', 'status', 'updated_at']
+    fields = ['task', 'status', 'updated_at', 'description']
 
 
 admin.site.register(Task, TaskAdmin)
--- sourse/webapp/models.py
+++ sourse/webapp/models.py
@@ -6,6 +6,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class Task(models.Model):
     task = models.CharField(max_length=200, null=False, blank=False, verbose_name="Задача")
     status = models.CharField(max_length=30, null=False, blank=False, default="new", choices=status_choices, verbose_name="Статус")
+    description = models.TextField(max_length=2000, null=True, blank=False, verbose_name="Описание")
     updated_at = models.DateField(auto_now=False,null=True, blank=False, verbose_name="Дедлайн")
 
     def __str__(self):
