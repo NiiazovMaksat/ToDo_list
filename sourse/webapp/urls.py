18787b3737a897ab7e2e733ad1f5d90e5afb531c
NiiazovMaksat <niyazov37@gmail.com>
2022-01-02 22:43:14 +0600

    добавил имена к ссылкав в ЮРЛках

--- sourse/webapp/urls.py
+++ sourse/webapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from webapp.views import create_view, list_view, todo_view
 
 urlpatterns = [
-    path('', list_view),
-    path('add/', create_view),
-    path('view/<int:pk>/', todo_view)
+    path('', list_view, name="index"),
+    path('add/', create_view, name="create"),
+    path('view/<int:pk>/', todo_view="view")
 ]
