14cfc88e200211f8b021d241724efc9636b9b627
NiiazovMaksat <niyazov37@gmail.com>
2022-01-06 04:23:08 +0600

    добавлена страница редактирования

--- sourse/webapp/views.py
+++ sourse/webapp/views.py
@@ -21,22 +21,25 @@ def create_view(request):
         status = request.POST.get('status')
         updated_at = request.POST.get('updated_at')
         new_task = Task.objects.create(task=task, status=status, updated_at=updated_at)
-        # url = reverse("view", kwargs={"pk": new_task.pk})
-        # return HttpResponseRedirect(url)
         return redirect("view", pk=new_task.pk)
 
 
 
 def todo_view(request, pk):
-    #try:
-    #   task = Task.objects.get(pk=pk)
-    #except Task.DoesNotExist:
-    #    return HttpResponseNotFound("Статья не найдена")
     task = get_object_or_404(Task, pk=pk)
     context = {"task": task}
     return render(request, 'view.html', context)
 
-def view_all(request, pk):
+def view_edit(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    context = {"task": task}
-    return render(request, 'view_all.html', context)
+    if request.method == 'GET':
+        return render(request, 'edit.html', {'task': task, 'status_choices': status_choices})
+    else:
+        task.task = request.POST.get('task')
+        task.status = request.POST.get('status')
+        task.description = request.POST.get('description')
+        task.updated_at = request.POST.get('updated_at')
+        task.save()
+
+        return redirect("view", pk=task.pk)
+
