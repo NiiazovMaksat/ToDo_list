653fed802fc04dedff304712fdf72ab3bc00c368
NiiazovMaksat <niyazov37@gmail.com>
2022-01-06 13:38:13 +0600

    форма изменения и удалил файл утилз

--- sourse/webapp/views.py
+++ sourse/webapp/views.py
@@ -6,8 +6,6 @@ from webapp.models import Task
 from webapp.models import status_choices
 
 
-from webapp.utils import task_validate
-
 def list_view(request):
     task = Task.objects.order_by("updated_at")
     return render(request, 'index.html', {'task': task})
@@ -40,20 +38,31 @@ def todo_view(request, pk):
 def view_edit(request, pk):
     task = get_object_or_404(Task, pk=pk)
 
+
     if request.method == 'GET':
-        return render(request, 'edit.html', {'task': task, 'status_choices': status_choices})
+        form = TaskForm(initial={
+            'task': task.task,
+            'status': task.status,
+            'description': task.description,
+            'updated_at': task.updated_at
+
+        })
+        return render(request, 'edit.html', {'task': task, 'status_choices': status_choices, 'form':form})
     else:
-        task.task = request.POST.get('task')
-        task.status = request.POST.get('status')
-        task.description = request.POST.get('description')
-        task.updated_at = request.POST.get('updated_at')
-        errors = task_validate(task.task, task.updated_at)
-        if errors:
-            return render(request, 'edit.html', {'status_choices': status_choices, 'errors': errors, 'task': task})
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task.task = form.cleaned_data.get('task')
+            task.status = form.cleaned_data.get('status')
+            task.description = form.cleaned_data.get('description')
+            task.updated_at = form.cleaned_data.get('updated_at')
+            task.save()
+            return redirect("view", pk=task.pk)
+        return render(request, 'edit.html', {'task': task, 'status_choices': status_choices, 'form': form})
+
+
 
 
         task.save()
-
         return redirect("view", pk=task.pk)
 
 def view_delete(request, pk):
